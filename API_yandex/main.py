48f48dbf38a4d781f7abb1d0b85eae2a7e8caf7f
Ovsienko023 <Ovsienko023@gmail.com>
2020-01-03 18:26:12 +0300

    add def tranlation

--- API_yandex/main.py
+++ API_yandex/main.py
@@ -2,12 +2,14 @@ import requests
 import json
 
 
-params = {'key': 'trnsl.1.1.20200101T170917Z.2023ca317ec893c7.e8549916eb75939aa6878a1e7a453489f4302654',
-          'text': ''}
-params['text'] = input()
-url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?lang=en-ru&text={text}&key={key}'
+def translation(word):
+    params = {'key': 'trnsl.1.1.20200101T170917Z.2023ca317ec893c7.e8549916eb75939aa6878a1e7a453489f4302654',
+            'text': ''}
+    params['text'] = word
+    url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?lang=en-ru&text={text}&key={key}'
 
 
-go = requests.get(url, params=params)
-data = json.loads(go.content)
-print(data['text'][1])
+    go = requests.get(url, params=params)
+    data = json.loads(go.content)
+    print(data['text'][1])
+    return data['text'][1]
