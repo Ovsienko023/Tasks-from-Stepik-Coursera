e8729884209b215b74fe105df2e6ff5e29621311
Ovsienko023 <Ovsienko023@gmail.com>
2020-03-30 20:36:38 +0300

    metod get in client - GOVNOKOD

--- Coursera/week_5_client.py
+++ Coursera/week_5_client.py
@@ -1,12 +1,10 @@
 import socket
+import time
 
 
-# with socket.create_connection(("127.0.0.1", 10001)) as sock:
-#     sock.sendall("ping".encode("utf8"))
-#     data = sock.recv(1024)
-#     print(data.decode("utf8"))
-
-
+class ClientError(Exception):
+    """ I'm Error classa Client! """
+    pass
 
 class Client:
     def __init__(self, host, port, timeout=None):
@@ -14,52 +12,68 @@ class Client:
         self.port = port
         self.timeout = timeout
         self.sock = socket.socket()
-        self.sock.connect(("127.0.0.1", 10001))
+        self.sock.connect((self.host, self.port))
 
 
-    def put(self, data):
+    def put(self, metrica, numb_val, timestamp=None):
         """ Data transfer to the server """
+
+        timestamp = timestamp or int(time.time())
+        data = 'put {} {} {}\n'.format(metrica, numb_val, timestamp)
         self.sock.sendall(data.encode("utf8"))
+        otvet = self.sock.recv(1024)
+        #print(otvet.decode("utf8"))
+        if otvet.decode('utf8') == "error\nwrong command\n\n":
+            raise ClientError
 
     
     def get(self, data_key):
         """ Receiving data from the server """
-        self.sock.sendall(data_key.encode("utf8"))
-        data_r = self.sock.recv(1024)
-        print(data_r.decode("utf8"))
+
+        if data_key == '*':
+            self.sock.sendall('get *\n'.encode("utf8"))
+            data_r = self.sock.recv(1024)
+        else:
+            data = 'get {}\n'.format(data_key)
+            self.sock.sendall(data.encode("utf8"))
+            data_r = self.sock.recv(1024)
+
+        if data_r.decode('utf8') == "error\nwrong command\n\n":
+            raise ClientError
+
+        if data_r.decode('utf8') == "ok\n\n":
+            return {}
+        else:
+            try:
+                fin_data = dict()
+                data_r = data_r.decode('utf8').split('\n')
+                for line in data_r:
+                    if line and (line not in 'ok'):
+                            metrica, numb_val, timestamp = line.split()
+                            #print(metrica, numb_val, timestamp)
+                            if fin_data.get(metrica):
+                                fin_data[metrica].append((int(timestamp), float(numb_val)))
+                            else:
+                                fin_data[metrica] = [(int(timestamp), float(numb_val))]
+
+                for met in fin_data:
+                    fin_data[met] = sorted(fin_data[met], key=lambda typ: typ[0])
+
+                return fin_data
+            except:
+                raise ClientError
     
-            
 
+    def close(self):
+        try:
+            self.sock.close()
+        except socket.error as err:
+            raise ClientError("Error. Do not close the connection", err)
 
-# sock = socket.socket() # Для соединения к серверу
-# sock.connect(("127.0.0.1", 10001))
-# sock.sendall("ping".encode("utf8"))
-# sock.close()
-
-client = Client("127.0.0.1", 10001)
-#client.put('!!!')
-client.get('key')
-
-client.put("hi")
-
-
-#### SERVER
-
-# import socket
-
-
-# with socket.socket() as sock:
-#     sock.bind(("", 10001))
-#     sock.listen(5)
-
-#     while True:
-#         coonect, addres = sock.accept()
-#         print(addres)
-#         with coonect:
-#             while True:
-#                 data = coonect.recv(1024)
-#                 if not data:
-#                     break
-#                 print(data.decode("utf8"))
-#                 coonect.send("value".encode("utf8"))
-    
+### TEST
+#client = Client("127.0.0.1", 10001)
+# #client.put("eardrum.memory", 4200000)
+# client.put('palm.cpu', 23.7, 1150864247)
+# print(client.get('palm.cpu'))
+# client.put("hi")
+#client = Client("127.0.0.1", 8888, timeout=15)
---
+++ Coursera/week_5_test_server.py
@@ -0,0 +1,24 @@
+import socket
+
+
+sock = socket.socket()
+sock.bind(('127.0.0.1', 10001))
+sock.listen(1)
+conn, addr = sock.accept()
+
+print('Соединение установлено:', addr)
+
+# переменная response хранит строку возвращаемую сервером, если вам для
+# тестирования клиента необходим другой ответ, измените ее
+response = b'ok\npalm.cpu 2.0 1150864248\npalm.cpu 0.5 1150864248\n\n'
+#response = b"error\nwrong command\n\n"
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    request = data.decode('utf-8')
+    print(f'Получен запрос: {ascii(request)}')
+    print(f'Отправлен ответ {ascii(response.decode("utf-8"))}')
+    conn.send(response)
+
+conn.close()
