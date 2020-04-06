1dae3c8b2f1cc0b70e746d0dc381e178e6cbefe0
Ovsienko023 <Ovsienko023@gmail.com>
2020-04-06 15:47:07 +0300

    new_6_task

--- Test/6_task_new.py
+++ Test/6_task_new.py
@@ -7,14 +7,14 @@ def matrixmult(a,b):
     new_matr=[]  
     final=[] 
     try:
-        for k in range(0,len(a)):
-            for j in range(0,len(b[0]) ):
-                for i in range(0,len(a[0])):
+        for k in range(0, len(a)):
+            for j in range(0, len(b[0])):
+                for i in range(0, len(a[0])):
                     sum_ = sum_ + a[k][i] * b[i][j]
                 new_matr.append(sum_)
-                sum_= 0
+                sum_ = 0
             final.append(new_matr)
-            new_matr=[]  
+            new_matr= []  
     except IndexError:
         print("Матрицы нельзя перемножить")         
     return final
