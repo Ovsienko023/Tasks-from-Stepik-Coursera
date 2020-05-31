89f7243e823033cfa873be17dc3e56eebc07cc86
Ovsienko023 <Ovsienko023@gmail.com>
2020-05-31 12:38:50 +0300

    Date JS

--- Stepik_JS/Arrays/myscript.js
+++ Stepik_JS/Arrays/myscript.js
@@ -8,9 +8,19 @@ function testArray(a, b) {
 
 // testArray([8, 1, 1, 7, 4, 0], [5, 8, 5, 4, 8])
 
-var myArray = [8, 1, 1, 7, 4, 0];
+// var myArray = [8, 1, 1, 7, 4, 0];
 
-console.log(myArray.sort());
+// console.log(myArray.sort());
 
-delete myArray[0]
-console.log(myArray);
+// delete myArray[0]
+// console.log(myArray);
+
+function testArray2(a, b) {
+    var lst_arr;
+    lst_arr = a.concat(b).split('');
+    lst_arr.unshift('Иванов');
+    lst_arr = lst_arr.reverse().join('')
+    return lst_arr;
+}
+document.write(testArray2('4326', '297515'))
+// console.log(testArray2('4326', '297515'))
